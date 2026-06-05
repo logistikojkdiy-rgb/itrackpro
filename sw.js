df0d4ff2797953c9553c87f64e92a3537f5894ac
Logistik ojkdiy <logistikojkdiy@gmail.com>
2026-06-05 10:06:35 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'intrack-cache-v3';
-const APP_ASSETS = ['./','./index.html'];
+const CACHE_NAME = 'itrack-cache-v4';
+const APP_ASSETS = ['./','./id.html'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))); self.clients.claim(); });
-self.addEventListener('fetch', event => { if (event.request.method !== 'GET') return; event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).catch(() => caches.match('./index.html')))); });
+self.addEventListener('fetch', event => { if (event.request.method !== 'GET') return; event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).catch(() => caches.match('./id.html')))); });
