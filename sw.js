5779dbe2e934547197d025058e4e8930651f27b5
Logistik ojkdiy <logistikojkdiy@gmail.com>
2026-06-07 15:42:36 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='itrack-pro-final-v120kb-20260607';
+const CACHE_NAME='itrack-blue-stable-20260607-v1';
 const ASSETS=['./','./id.html','./manifest.webmanifest'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;const url=new URL(event.request.url);if(url.hostname.includes('script.google.com'))return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(res=>{const copy=res.clone();if(res.ok&&url.origin===location.origin)caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));return res;}).catch(()=>caches.match('./id.html'))));});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;const url=new URL(event.request.url);if(url.hostname.includes('script.google.com'))return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).catch(()=>caches.match('./id.html'))));});
