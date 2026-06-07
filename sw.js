633000e8ec536b215279977c25f35d4749f5b605
Logistik ojkdiy <logistikojkdiy@gmail.com>
2026-06-07 23:15:20 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='itrack-stage4-enterprise-final-v1';
+const CACHE_NAME='itrack-stage4-preserve-ui-fixed-v1';
 const ASSETS=['./','./id.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
